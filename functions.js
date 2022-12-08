93a456d756f5e2bf227a62e3a60c2c1db7ddf2a8
enginbzky <enginbozkaya1@gmail.com>
2022-12-08 13:19:33 +0100

    4 tests  were completed

--- functions.js
+++ functions.js
@@ -1,3 +1,35 @@
-const isTheWordSplit = () => {};
+const isTheWordSplit = (pArray) => {
+  // "firstElement" is first element of pArray
+  let firstElement = pArray[0];
+  // "dictionary" is second element of pArray
+  let dictionary = pArray[1];
+
+  // "singleString" is a new array created from dictionary
+  let singleStrings = dictionary.split(",");
+  // "answerWords" is holding "isTheWordSplit" function's output
+  let answerWords = "";
+
+  // Here I used nested .map method to combine singleStrings elements to match with "firstElement"
+  singleStrings.map((firstWord) => {
+    singleStrings.map((word) => {
+      let joinedWord = firstWord + word;
+      let reversedWord = [joinedWord].reverse().toString();
+
+      if (joinedWord == firstElement || reversedWord == firstElement) {
+        return (answerWords = firstWord + "," + word);
+      } else {
+        return "not possible";
+      }
+    });
+  });
+  /* Normally the if/else condition above should have given correct results. 
+  But the else condition returns an empty element (""), 
+  Because of that I created this if/else condition. So I cheated :))*/
+  if (answerWords == "") {
+    return "not possible";
+  } else {
+    return answerWords;
+  }
+};
 
 export { isTheWordSplit };
--- functions.test.js
+++ functions.test.js
@@ -1,30 +1,30 @@
-import { isTheWordSplit } from './functions.js';
+import { isTheWordSplit } from "./functions.js";
 
-test('According to the given parameter, the result should be hello,cat', () => {
+test("According to the given parameter, the result should be hello,cat", () => {
   let result = isTheWordSplit([
-    'hellocat',
-    'apple,bat,cat,goodbye,hello,yellow,why',
+    "hellocat",
+    "apple,bat,cat,goodbye,hello,yellow,why",
   ]);
-  expect(result).toBe('hello,cat');
+  expect(result).toBe("hello,cat");
 });
 
-test('According to the given parameter, the result should be base,ball', () => {
+test("According to the given parameter, the result should be base,ball", () => {
   let result = isTheWordSplit([
-    'baseball',
-    'a,all,b,ball,bas,base,cat,code,d,e,quit,z',
+    "baseball",
+    "a,all,b,ball,bas,base,cat,code,d,e,quit,z",
   ]);
-  expect(result).toBe('base,ball');
+  expect(result).toBe("base,ball");
 });
 
-test('According to the given parameter, the result should be abcg,efd', () => {
-  let result = isTheWordSplit(['abcgefd', 'a,ab,abc,abcg,b,c,dog,e,efd,zzzz']);
-  expect(result).toBe('abcg,efd');
+test("According to the given parameter, the result should be abcg,efd", () => {
+  let result = isTheWordSplit(["abcgefd", "a,ab,abc,abcg,b,c,dog,e,efd,zzzz"]);
+  expect(result).toBe("abcg,efd");
 });
 
-test('According to the given parameter, the result should be not possible', () => {
+test("According to the given parameter, the result should be not possible", () => {
   let result = isTheWordSplit([
-    'hicoders',
-    'code,coder,hi,hicode,hicoder,hicoders',
+    "hicoders",
+    "code,coder,hi,hicode,hicoder,hicoders",
   ]);
-  expect(result).toBe('not possible');
+  expect(result).toBe("not possible");
 });
